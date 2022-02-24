78122253b3e69c756835704971346a2a6841dbb9
bibirock <bibircok0104@gmail.com>
2022-02-24 20:24:57 +0800

    ok

--- html/index/index.js
+++ html/index/index.js
@@ -1,16 +1,35 @@
+function doQuery(){
+  // 側邊欄點擊
+  $(function () {
+    // 點擊
+    $("div.img").on("click", function () {
+      // -open 的樣式切換
+      $("aside").toggleClass("-open"), $("div.light").toggleClass("-on");
+    });
+  });
+  $(function () {
+    // 點擊
+    $("img.x").on("click", function (e) {
+      // -open 的樣式切換
+      $("aside").removeClass("-open"), $("div.light").removeClass("-on");
+      
+    });
+  $(window).on("click",(e) =>{
+    const light = document.querySelector(".light");
+    if(e.target == light){
+      $("aside").removeClass("-open"), $("div.light").removeClass("-on");
+      };
+    })
+  });
+}
+window.addEventListener("load", doQuery);
 
-// 側邊欄點擊
-$(function () {
-  // 點擊
-  $("div.img").on("click", function () {
-    // -open 的樣式切換
-    $("aside").toggleClass("-open"), $("div.light").toggleClass("-on");
-  });
-});
-$(function () {
-  // 點擊
-  $("img.x").on("click", function () {
-    // -open 的樣式切換
-    $("aside").removeClass("-open"), $("div.light").removeClass("-on");
-  });
-});
+export {
+  doQuery
+};
+
+
+
+
+// const observer = new IntersectionObserver(callback, options);
+ 
